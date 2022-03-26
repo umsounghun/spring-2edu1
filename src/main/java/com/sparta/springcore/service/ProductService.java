455fe0a4c1a6450a21c5793cd09868ccd0630c9a
Sounghun Um <korea970112@naver.com>
2022-03-26 16:12:17 +0900

    2주차 11강 관리자 상품 조회

--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -1,12 +1,12 @@
 package com.sparta.springcore.service;
 
-import com.sparta.springcore.model.Product;
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
-import com.sparta.springcore.repository.ProductRepository;
 import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -14,20 +14,21 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
 // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
+        Product product = new Product(requestDto, userId);
 
         productRepository.save(product);
 
         return product;
     }
 
-    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
 
@@ -38,12 +39,13 @@ public class ProductService {
         return product;
     }
 
-    private void orElseTrhow(Object o) {
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
     }
 
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = productRepository.findAll();
-
-        return products;
+    // 모든 상품 조회 (관리자용)
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
